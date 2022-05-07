12b9c51ca11f43af32ab8f3801bba562f1403886
Gianluca Zappavigna <gianluca.zappavigna@gmail.com>
2022-05-07 18:29:17 +0200

    progress

---
+++ launch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# shellcheck disable=SC1143
+
+n_runs=11
+
+find ensemble/ -regextype posix-extended -regex "^.*/[0-9]+\.pdb$" \
+    | shuf -n $n_runs \
+    | xargs -t python make_input_conf.py  \
+        --output-dir runs/ \
+        --n-tasks 1 \
+        --lmp-path /home/gzappavigna/lammps/build/lmp_custom \
+        # --use-cmap \
+        # --ens-traj /data/vschnapka/202310-CMAP-HPS/MeV-NT/MeV_NT_ens/ensemble_200_1/ensemble_200_1.xtc \
+        # --target-dist /data/vschnapka/202310-CMAP-HPS/reference_dih/all-rama-ref.out \
+        # --topo-pdb CG.pdb
